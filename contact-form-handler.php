468024d10dbbe6aab111ff306c3e4fb39cc950f3
Luis O <49507652+luis1096@users.noreply.github.com>
2022-03-31 02:06:13 -0400

    Add files via upload

---
+++ contact-form-handler.php
@@ -0,0 +1,14 @@
+<?php
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $message = $_POST["message"];
+    $email_from = "luisqaz222@gmail.com"
+    $email_subject = "New Form Submission"
+    $email_body = "User Name: $name\nUser Email: $email\n
+    User Message: $message\n";
+    $to = "loliv061@fiu.edu";
+    $header = "From: $email_from\r\n Reply-To: $email\r\n";
+
+    mail($to, $email_subject, $email_body, $header);
+    header("Location: index.html");
+?>
