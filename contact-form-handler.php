a2b263af2a14ff302b703c2f3b16b08ac30e6881
Luis O <49507652+luis1096@users.noreply.github.com>
2022-03-31 02:16:59 -0400

    Update contact-form-handler.php

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -2,8 +2,8 @@
     $name = $_POST["name"];
     $email = $_POST["email"];
     $message = $_POST["message"];
-    $email_from = "luisqaz222@gmail.com"
-    $email_subject = "New Form Submission"
+    $email_from = "luisqaz222@gmail.com";
+    $email_subject = "New Form Submission";
     $email_body = "User Name: $name\nUser Email: $email\n
     User Message: $message\n";
     $to = "loliv061@fiu.edu";
